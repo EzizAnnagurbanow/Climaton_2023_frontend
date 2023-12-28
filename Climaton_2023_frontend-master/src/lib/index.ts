7861687de95d9a99aac6253affa8c39d81b27879
EzizAnnagurbanow <100955831+EzizAnnagurbanow@users.noreply.github.com>
2023-12-28 22:29:23 +0500

    Delete Climaton_2023_frontend-master directory

--- Climaton_2023_frontend-master/src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-export default prisma
-
-export const getLocalTimestampInSeconds = () => {
-    const now = new Date();
-	return Math.round((now.getTime() - now.getTimezoneOffset() * 60000) / 1000);
-}
--- Climaton_2023_frontend-master/src/routes/+server.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('./$types').RequestHandler} */
-import prisma from '$lib';
-import { json } from '@sveltejs/kit';
-
-export async function GET() {
-    const members = await prisma.member.findMany()
-	return json(members)
-}
